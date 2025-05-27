d6035dfe10b4f7818be601492855d1f6c576c641
letmemeow <194444226+letmemeow@users.noreply.github.com>
2025-05-27 19:27:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-yay -S hyprland kitty thunar wofi wlogout waybar swww slurp grim micro qview gthumb nwg-look btop pavucontrol 
+yay -S hyprland kitty nemo wofi wlogout waybar swww slurp grim micro qview gthumb nwg-look btop pavucontrol 
