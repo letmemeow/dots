f8ad189e071c31802ca2e4d94d0761aabeb816cc
letmemeow <194444226+letmemeow@users.noreply.github.com>
2025-05-31 19:05:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ BACKUP_DIR="$HOME/.config-backup-$(date +%Y%m%d-%H%M%S)"
 
 REQUIRED_PKGS=(
   hyprland kitty nemo wofi wlogout waybar swww slurp grim
-  micro qview gthumb nwg-look btop pavucontrol waypaper
+  micro qview gthumb nwg-look btop pavucontrol waypaper wl-clipboard cliphist
 )
 
 echo "📦 Updating system..."
